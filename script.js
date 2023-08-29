f96f9e7b8b744192e86564c2c434c2bdabdc3d8a
MAXIM <117771444+maaax1k@users.noreply.github.com>
2023-08-29 09:02:23 +0600

    new

---
+++ script.js
@@ -0,0 +1,35 @@
+function calculate(){
+  var result = 0;
+  var kcal = document.getElementById('kkal');
+  var brf = document.getElementById('brf');
+  var fit = document.getElementById('fit');
+  var book = document.getElementById('book');
+  good = document.getElementById('good');
+  var sum = Number (document.getElementById('sum').value) - 1000;
+  if(!kcal.checked){
+    result += 500;
+  }
+  if(!brf.checked){
+    result += 400;
+  }
+  if(!fit.checked){
+    result += 500;
+  }
+  if(sum>0){
+    result +=sum;
+  }
+  
+  if(book.checked){
+    document.getElementById('result1').textContent = 'Music+';
+  }
+  else{
+    document.getElementById('result1').textContent = 'Music-';
+  }
+  if(good.checked){
+    document.getElementById('result2').textContent = 'Cs and Videos+';
+  }
+  else{
+    document.getElementById('result2').textContent = 'Cs and Videos-';
+  }
+  document.getElementById('result').textContent = result + ' tg';
+}
